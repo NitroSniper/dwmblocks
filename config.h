7f7f858ff1f6cc45f717d892b89081486d66ce5c
th13rry <thierrycatrain@gmail.com>
2023-05-05 19:28:59 +0200

    Cleaning residual tabs

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"󰏗 ",    "dwmblocks-updates",  3600,               17},
-	{"󰕾 ",    "dwmblocks-volume",   0,                  10},
-	{"",      "dwmblocks-wifi",     3,                  0},
-	{"" ,     "dwmblocks-clock",    60,                 0},
+  /*Icon*/  /*Command*/    /*Update Interval*/  /*Update Signal*/
+  {"󰏗 ",    "dwmblocks-updates",  3600,               17},
+  {"󰕾 ",    "dwmblocks-volume",   0,                  10},
+  {"",      "dwmblocks-wifi",     3,                  0},
+  {"" ,     "dwmblocks-clock",    60,                 0},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
