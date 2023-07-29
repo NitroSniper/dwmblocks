639176bc29dc835d5653cf663a15caab5a4c961b
th13rry <thierrycatrain@gmail.com>
2023-07-29 23:37:15 +0200

    Lazy loading updates

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon*/  /*Command*/    /*Update Interval*/  /*Update Signal*/ /*Is Lazy*/
-  {"󰏗 ",    "dwmblocks-updates",  3600,               17,             1},
+  {"󰏗 ",    "dwmblocks-updates",  60,                 17,             1},
   {"󰕾 ",    "dwmblocks-volume",   0,                  10,             0},
-  {"",      "dwmblocks-wifi",     1,                  0,              0},
+  {"",      "dwmblocks-wifi",     60,                  0,              0},
   {"" ,     "dwmblocks-clock",    60,                 0,              0}
 };
 
